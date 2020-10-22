1bb9d01683295593322d7dc70066623226d12a3b
Sung M. Kim <sungmkim80@gmail.com>
2020-10-22 02:10:50 -0400

    Removed postcss deps

--- index.js
+++ index.js
@@ -16,36 +16,43 @@ module.exports = function (_, { id, ...options }) {
           rules: [
             {
               test: /\.css$/i,
-              oneOf: [
+              use: [
+                // ...getStyleLoaders(isServer),
                 {
-                  test: /\.module\.s[ca]ss$/,
-                  use: [
-                    ...getStyleLoaders(isServer, {
-                      modules: {
-                        localIdentName: isProd
-                          ? `[local]_[hash:base64:4]`
-                          : `[local]_[path]`,
-                      },
-                      importLoaders: 1,
-                      sourceMap: !isProd,
-                      onlyLocals: isServer,
-                    }),
-                    {
-                      loader: "postcss-loader",
-                      options: options ?? {},
-                    },
-                  ],
-                },
-                {
-                  use: [
-                    ...getStyleLoaders(isServer),
-                    {
-                      loader: "postcss-loader",
-                      options: options ?? {},
-                    },
-                  ],
+                  loader: "postcss-loader",
+                  options: options || {},
                 },
               ],
+              // oneOf: [
+              //   {
+              //     test: /\.module\.s[ca]ss$/,
+              //     use: [
+              //       ...getStyleLoaders(isServer, {
+              //         modules: {
+              //           localIdentName: isProd
+              //             ? `[local]_[hash:base64:4]`
+              //             : `[local]_[path]`,
+              //         },
+              //         importLoaders: 1,
+              //         sourceMap: !isProd,
+              //         onlyLocals: isServer,
+              //       }),
+              //       {
+              //         loader: "postcss-loader",
+              //         options: options || {},
+              //       },
+              //     ],
+              //   },
+              //   {
+              //     use: [
+              //       ...getStyleLoaders(isServer),
+              //       {
+              //         loader: "postcss-loader",
+              //         options: options || {},
+              //       },
+              //     ],
+              //   },
+              // ],
             },
           ],
         },
